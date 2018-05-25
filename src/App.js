690cf135ec8d7c55ce95ebb0bf8d1213c3c1b5ba
Git-Justin <justin5197@github.com>
2018-05-24 23:35:02 -0400

    added menu bar, styled menu bar, fixed error in paralax CSS code snippet

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello from Mason</h1>
-        </header>
-        <p className="App-intro">
-          Hello world!
-        </p>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <h1 className="App-title">Wesleys Mobile Device Repair</h1>
+          </header>
+          <div className="navBar">
+            <ul>
+              <li><a class="active" href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#repairs">Repairs</a></li>
+              <li><a href="#contact">Contact</a></li>
+
+            </ul>
+          </div>
+          <div className="parallax"></div>
+        </div>
+
     );
   }
 }
